653a556f7e4c28190f3d8afe69282f61cce0835b
Jorge Urquijo <jorgedzo_17@hotmail.com>
2016-10-07 06:26:44 -0700

    URL API
    Se cambio Url de API a servidor IIS Virutual

--- service/dxControles/dxControles.js
+++ service/dxControles/dxControles.js
@@ -8,8 +8,13 @@ angular.module('ControlesAngular')
         var ACTION_SAVE = "/Controles/Guardar/";
         var ACTION_GET = "/Controles/Get/";
         var ACTION_DATASOURCE = "/DataSource/Get/";
+<<<<<<< Updated upstream
         // var API_URL = 'http://localhost:64632/Api';
         var API_URL = 'http://localhost/Controles/Api';
+=======
+        //        var API_URL = 'http://localhost:64632/Api';
+        var API_URL = 'http://192.168.33.12/Controles/Api';
+>>>>>>> Stashed changes
 
         var varObjDX = {};
 
@@ -19,16 +24,16 @@ angular.module('ControlesAngular')
         // var dataSourceCustomize = new DataSource({ store: [], paginate: false });
 
         var enumTipos = {
-            Texto: 1,
-            Numerico: 2,
-            Fecha: 3,
-            CheckBoxList: 4,
-            RadioButtonList: 5,
-            List: 6,
-            Multiseleccion: 7,
-            Switch: 8,
-            TextArea: 9,
-            SubirDocumentos: 10
+            Texto: 1
+            , Numerico: 2
+            , Fecha: 3
+            , CheckBoxList: 4
+            , RadioButtonList: 5
+            , List: 6
+            , Multiseleccion: 7
+            , Switch: 8
+            , TextArea: 9
+            , SubirDocumentos: 10
         };
 
         dxControles.getDataSource = function (UUID, Key) {
@@ -45,10 +50,10 @@ angular.module('ControlesAngular')
                     console.log(objSectionDX);
 
                     return dxControles.formOptions = {
-                        colCount: 1,
-                        labelLocation: 'top',
-                        items: dxControles.Sections,
-                    };
+                        colCount: 1
+                        , labelLocation: 'top'
+                        , items: dxControles.Sections
+                    , };
 
                 }, function (error) {
                     return error;
@@ -61,11 +66,11 @@ angular.module('ControlesAngular')
             var index;
             for (var i in data) {
                 varObjDX = {
-                    caption: data[i].Nombre,
-                    title: data[i].Nombre,
-                    colCount: data[i].Columnas,
-                    IdGrupo: data[i].IdGrupo,
-                    icon: data[i].Icono
+                    caption: data[i].Nombre
+                    , title: data[i].Nombre
+                    , colCount: data[i].Columnas
+                    , IdGrupo: data[i].IdGrupo
+                    , icon: data[i].Icono
                 };
 
                 varObjDX.items = [];
@@ -81,21 +86,17 @@ angular.module('ControlesAngular')
                     index = dxControles.buscarIndex(varObjDX.IdGrupo);
                     if (index !== undefined) {
                         dxControles.Sections[index].tabs.push(varObjDX);
-                    }
-                    else {
-                        dxControles.Sections.push(
-                            {
-                                itemType: 'tabbed',
-                                IdGrupo: varObjDX.IdGrupo,
-                                tabs: [varObjDX],
-                                tabPanelOptions: {///Hace que se cargue todo aunque el tab no se este mostrando
-                                    deferRendering: false
-                                }
+                    } else {
+                        dxControles.Sections.push({
+                            itemType: 'tabbed'
+                            , IdGrupo: varObjDX.IdGrupo
+                            , tabs: [varObjDX]
+                            , tabPanelOptions: { ///Hace que se cargue todo aunque el tab no se este mostrando
+                                deferRendering: false
                             }
-                        );
+                        });
                     }
-                }
-                else {
+                } else {
                     varObjDX.itemType = 'group';
                     dxControles.Sections.push(varObjDX);
                 }
@@ -111,20 +112,20 @@ angular.module('ControlesAngular')
             var controlDX = {
                 label: {
                     text: data.Nombre
-                },
-                dataField: data.IdSeccionControl,
-                isRequired: data.Requerido,
+                }
+                , dataField: data.IdSeccionControl
+                , isRequired: data.Requerido,
 
-                hint: data.Caption,
-                editorOptions: {
-                    placeholder: data.Caption,
-                    maxLength: data.Longitud,
-                    value: data.ValorDefault,
-                    name: data.NombreColumna,
-                    control: data.IdTipoControl
-                },
-                cssClass: data.CssClass,
-                helpText: data.HelpText
+                hint: data.Caption
+                , editorOptions: {
+                    placeholder: data.Caption
+                    , maxLength: data.Longitud
+                    , value: data.ValorDefault
+                    , name: data.NombreColumna
+                    , control: data.IdTipoControl
+                }
+                , cssClass: data.CssClass
+                , helpText: data.HelpText
             };
 
             controlDX = dxControles.getEditorType(data, controlDX);
@@ -138,99 +139,96 @@ angular.module('ControlesAngular')
 
             if (varObjDX.isRequired) {
                 varObjDX.validationRules = [{
-                    type: "required",
-                    message: "Campo Requerido"
+                    type: "required"
+                    , message: "Campo Requerido"
                 }];
             }
 
             switch (objControl.IdTipoControl) {
-                case enumTipos.Texto:
-                    strTipo = "dxTextBox";
-                    break;
-                case enumTipos.Numerico:
-                    strTipo = "dxNumberBox";
-                    break;
-                case enumTipos.Fecha:
-                    strTipo = "dxDateBox";
-                    varObjDX.editorOptions.width = '100%';
-                    varObjDX.validationRules = [{
-                        type: "required",
-                        message: "Seleccione una Fecha"
+            case enumTipos.Texto:
+                strTipo = "dxTextBox";
+                break;
+            case enumTipos.Numerico:
+                strTipo = "dxNumberBox";
+                break;
+            case enumTipos.Fecha:
+                strTipo = "dxDateBox";
+                varObjDX.editorOptions.width = '100%';
+                varObjDX.validationRules = [{
+                    type: "required"
+                    , message: "Seleccione una Fecha"
                     }];
-                    break;
-                case enumTipos.CheckBoxList:
-                    strTipo = "dxTagBox";
+                break;
+            case enumTipos.CheckBoxList:
+                strTipo = "dxTagBox";
 
-                    break;
-                case enumTipos.RadioButtonList:
-                    strTipo = "dxRadioGroup";
-                    varObjDX.editorOptions.dataSource = objControl.ObjDataSource.DataSource;
-                    varObjDX.editorOptions.valueExpr = objControl.ObjDataSource.ValueField;
-                    varObjDX.editorOptions.displayExpr = objControl.ObjDataSource.TextField;
-                    break;
-                case enumTipos.List:
-                    strTipo = "dxSelectBox";
-                    varObjDX.editorOptions.dataSource = objControl.ObjDataSource.DataSource;
-                    varObjDX.editorOptions.valueExpr = objControl.ObjDataSource.ValueField;
-                    varObjDX.editorOptions.displayExpr = objControl.ObjDataSource.TextField;
-                    varObjDX.editorOptions.searchEnabled = true;
-                    varObjDX.validationRules.push(
-                        {
-                            type: "pattern",
-                            pattern: "[1-9]",
-                            message: "Seleccione un Campo."
-                        });
-                    var idControl = objControl.IdSeccionControl;
-                    var idPadre = objControl.IdSeccionControlPadre;
-                    var value = objControl.ValorDefault;
+                break;
+            case enumTipos.RadioButtonList:
+                strTipo = "dxRadioGroup";
+                varObjDX.editorOptions.dataSource = objControl.ObjDataSource.DataSource;
+                varObjDX.editorOptions.valueExpr = objControl.ObjDataSource.ValueField;
+                varObjDX.editorOptions.displayExpr = objControl.ObjDataSource.TextField;
+                break;
+            case enumTipos.List:
+                strTipo = "dxSelectBox";
+                varObjDX.editorOptions.dataSource = objControl.ObjDataSource.DataSource;
+                varObjDX.editorOptions.valueExpr = objControl.ObjDataSource.ValueField;
+                varObjDX.editorOptions.displayExpr = objControl.ObjDataSource.TextField;
+                varObjDX.editorOptions.searchEnabled = true;
+                varObjDX.validationRules.push({
+                    type: "pattern"
+                    , pattern: "[1-9]"
+                    , message: "Seleccione un Campo."
+                });
+                var idControl = objControl.IdSeccionControl;
+                var idPadre = objControl.IdSeccionControlPadre;
+                var value = objControl.ValorDefault;
 
-                    if (idPadre !== 0) {
-                        var control =
-                            {
-                                IdSeccionControl: idControl,
-                                IdSeccionControlPadre: idPadre,
-                                value: value
-                            };
-                        padreHijo.push(control);
+                if (idPadre !== 0) {
+                    var control = {
+                        IdSeccionControl: idControl
+                        , IdSeccionControlPadre: idPadre
+                        , value: value
+                    };
+                    padreHijo.push(control);
 
-                        //     if (varObjDX.editorOptions.dataSource == null) {
-                        //         varObjDX.editorOptions.dataSource = dataSourceCustomize;
-                        //     }
-                        //     else {
-                        //         dataSourceCustomize.store()._array = [];
-                        //         for (var obj in objControl.ObjDataSource.DataSource) {
-                        //             //Alimenta el DataSourceCustomize con el resultado que arroja la API
-                        //             dataSourceCustomize.store().insert(objControl.ObjDataSource.DataSource[obj]);
-                        //         }
-                        //         dataSourceCustomize.load();
-                        //         varObjDX.editorOptions.dataSource = dataSourceCustomize;
-                        //     }
-                    }
+                    //     if (varObjDX.editorOptions.dataSource == null) {
+                    //         varObjDX.editorOptions.dataSource = dataSourceCustomize;
+                    //     }
+                    //     else {
+                    //         dataSourceCustomize.store()._array = [];
+                    //         for (var obj in objControl.ObjDataSource.DataSource) {
+                    //             //Alimenta el DataSourceCustomize con el resultado que arroja la API
+                    //             dataSourceCustomize.store().insert(objControl.ObjDataSource.DataSource[obj]);
+                    //         }
+                    //         dataSourceCustomize.load();
+                    //         varObjDX.editorOptions.dataSource = dataSourceCustomize;
+                    //     }
+                }
 
-                    if (isNaN(objControl.ValorDefault)) {
-                        varObjDX.editorOptions.value = objControl.ValorDefault;
-                    }
-                    else {
-                        varObjDX.editorOptions.value = parseInt(objControl.ValorDefault);
-                    }
+                if (isNaN(objControl.ValorDefault)) {
+                    varObjDX.editorOptions.value = objControl.ValorDefault;
+                } else {
+                    varObjDX.editorOptions.value = parseInt(objControl.ValorDefault);
+                }
 
-                    break;
-                case enumTipos.Multiseleccion:
-                    strTipo = "dxTagBox";
-                    break;
-                case enumTipos.Switch:
-                    strTipo = "dxSwitch";
-                    varObjDX.editorOptions.onText = objControl.TextoSeleccionado;
-                    varObjDX.editorOptions.offText = objControl.TextoNoSeleccionado;
-                    break;
-                case enumTipos.TextArea:
-                    strTipo = "dxTextArea";
-                    varObjDX.editorOptions.height = objControl.TextAreaHeight;
+                break;
+            case enumTipos.Multiseleccion:
+                strTipo = "dxTagBox";
+                break;
+            case enumTipos.Switch:
+                strTipo = "dxSwitch";
+                varObjDX.editorOptions.onText = objControl.TextoSeleccionado;
+                varObjDX.editorOptions.offText = objControl.TextoNoSeleccionado;
+                break;
+            case enumTipos.TextArea:
+                strTipo = "dxTextArea";
+                varObjDX.editorOptions.height = objControl.TextAreaHeight;
 
-                    break;
-                default:
-                    strTipo = "dxTextBox";
-                    break;
+                break;
+            default:
+                strTipo = "dxTextBox";
+                break;
             }
 
             varObjDX.editorType = strTipo;
